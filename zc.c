49915e82256d68e8d00694e6ef6443a95ae35615
衍星居士 <visus@qq.com>
2023-04-21 16:15:57 +0800

    添加求值命令e和编译命令c

--- zc.c
+++ zc.c
@@ -3,7 +3,7 @@
 #include "zc.h"
 
 void help() {
-  printf("【用法】：./zc h|v|<源码>\n");
+  printf("【用法】：./zc h|v|e <源码>|c <源码>\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -13,13 +13,18 @@ int main(int argc, char *argv[]) {
   }
 
   char *cmd = argv[1];
-  if (strcmp(cmd, "h") == 0) {
+  if (strcmp(cmd, "h") == 0) { // help
     help();
-  } else if (strcmp(cmd, "v") == 0) {
+  } else if (strcmp(cmd, "v") == 0) { //  version
     printf("Z语言编译期，版本号：%s。\n", ZC_VERSION);
-  } else {
+  } else if (strcmp(cmd, "e") == 0) { // eval
     char *src = cmd;
     eval(src);
+  } else if (strcmp(cmd, "c") == 0) { // compile
+    char *src = cmd;
+    compile(src);
+  } else {
+    help();
   }
 
   return 0;
@@ -27,5 +32,10 @@ int main(int argc, char *argv[]) {
 
 // 表达式求值
 void eval(char *src) {
-  printf("%s\n", src);
+  printf("eval> %s\n", src);
+}
+
+// 表达式编译
+void compile(char *src) {
+  printf("comp> %s\n", src);
 }
--- zc.h
+++ zc.h
@@ -5,3 +5,6 @@ static const char *ZC_VERSION = "0.0.1";
 
 // 表达式求值
 void eval(char *src);
+
+// 表达式编译
+void compile(char *src);
